e3670cd3b980a58d2ed6b1bd39c5a5e31c3a45ce
bekzod818 <bekzod030900@gmail.com>
2021-08-06 00:06:05 +0500

    improvment 2

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView, ProfileView, \
-    ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike, UserSearch
+    ProfileEditView, AddFollower, RemoveFollower, AddLike, AddDislike, UserSearch, ListFollowers
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
@@ -12,6 +12,7 @@ urlpatterns = [
     path('post/<int:pk>/dislike/', AddDislike.as_view(), name='dislike'),
     path('profile/<int:pk>/', ProfileView.as_view(), name="profile"),
     path('profile/edit/<int:pk>/', ProfileEditView.as_view(), name="profile-edit"),
+    path('profile/<int:pk>/followers/', ListFollowers.as_view(), name="list-followers"),
     path('profile/<int:pk>/followers/add/', AddFollower.as_view(), name='add-follower'),
     path('profile/<int:pk>/followers/remove/', RemoveFollower.as_view(), name='remove-follower'),
     path('search/', UserSearch.as_view(), name='profile-search'),
--- social/views.py
+++ social/views.py
@@ -240,3 +240,16 @@ class UserSearch(View):
         }
 
         return render(request, 'social/search.html', context)
+
+
+class ListFollowers(View):
+    def get(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        followers = profile.followers.all()
+
+        context = {
+            'profile': profile,
+            'followers': followers,
+        }
+
+        return render(request, "social/followers_list.html", context)
