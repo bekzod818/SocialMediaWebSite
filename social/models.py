7b86f156ab60c66f09a71ec1a1c3c599c50a7183
bekzod818 <bekzod030900@gmail.com>
2021-08-06 15:21:06 +0500

    replying to comments

--- social/models.py
+++ social/models.py
@@ -25,6 +25,19 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name='comment_likes')
+    dislikes = models.ManyToManyField(User, blank=True, related_name='comment_dislikes')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='+')
+
+    @property
+    def children(self):
+        return Comment.objects.filter(parent=self).order_by('-created_on').all()
+
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
 
     class Meta:
         ordering = ['-created_on']
